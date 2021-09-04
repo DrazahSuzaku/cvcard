f9f0ef58a9f31bd91fefe119c07bd18a3967e18c
drazah <drazahsuzaku@gmail.com>
2021-09-04 11:53:51 +0200

    Corrections Graphisme 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,5 @@ module.exports = {
     mode: 'jit',
     purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
     theme: {
-        fontFamily: {
-            'sans': ['"Roboto"', 'sans-serif'],
-        },
-
     }
 };
